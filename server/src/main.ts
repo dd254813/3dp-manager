ff7b1646b84edc0a64b1faed33a9543e6af9cfb1
Den Piligrim <89912505+vasiljevdenis@users.noreply.github.com>
2026-02-01 12:50:45 +0300

    nest proto

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { AuthService } from './auth/auth.service';
 import { RequestMethod } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
 
   const authService = app.get(AuthService);
   await authService.seedAdmin();
