702e4e3754053bfafe19dfee04d91ffdc2e606ac
dd254813 <43134945+dd254813@users.noreply.github.com>
2026-04-17 21:32:29 +0500

    Track inbound records in xui module for panel cleanup

--- server/src/xui/xui.module.ts
+++ server/src/xui/xui.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { Inbound } from '../inbounds/entities/inbound.entity';
 import { Setting } from '../settings/entities/setting.entity';
 import { XuiPanel } from './entities/xui-panel.entity';
 import { XuiPanelsService } from './xui-panels.service';
 import { XuiService } from './xui.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([XuiPanel, Setting])],
+  imports: [TypeOrmModule.forFeature([XuiPanel, Setting, Inbound])],
   providers: [XuiService, XuiPanelsService],
   exports: [XuiService, XuiPanelsService],
 })
