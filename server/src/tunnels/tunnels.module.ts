1111f944a2a030d468d943e44ebc7cc3d4c42104
dd254813 <43134945+dd254813@users.noreply.github.com>
2026-04-17 22:18:47 +0500

    Inject panel and inbound repositories into tunnels module

--- server/src/tunnels/tunnels.module.ts
+++ server/src/tunnels/tunnels.module.ts
@@ -3,12 +3,14 @@ import { TunnelsService } from './tunnels.service';
 import { TunnelsController } from './tunnels.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Tunnel } from './entities/tunnel.entity';
-import { Setting } from '../settings/entities/setting.entity';
+import { Inbound } from '../inbounds/entities/inbound.entity';
+import { XuiPanel } from '../xui/entities/xui-panel.entity';
 import { SshService } from './ssh.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Tunnel, Setting])],
+  imports: [TypeOrmModule.forFeature([Tunnel, Inbound, XuiPanel])],
   controllers: [TunnelsController],
   providers: [TunnelsService, SshService],
+  exports: [TunnelsService],
 })
 export class TunnelsModule {}
